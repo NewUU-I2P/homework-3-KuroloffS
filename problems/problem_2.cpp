68d5347e3022743f4d95b84435173b9597b0d013
KuroloffS <147370742+KuroloffS@users.noreply.github.com>
2023-11-14 19:55:57 +0500

    Update problem_2.cpp

--- problems/problem_2.cpp
+++ problems/problem_2.cpp
@@ -1,14 +1,7 @@
 float problemSolution2(float i, float j, float k) {
     float result;
     // write your code here
-    std::cin >> i;
-
-    std::cin >> j;
-
-    std::cin >> k;
-
     double result = (i > j) ? ((i > k) ? i : k) : ((j > k) ? j : k);
-    std::cout << result;
 
     return result;
 }
