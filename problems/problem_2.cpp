2c5152dde6ec3e690e19c80668819cb6d6df0fb0
KuroloffS <147370742+KuroloffS@users.noreply.github.com>
2023-11-14 19:43:13 +0500

    Update problem_2.cpp

--- problems/problem_2.cpp
+++ problems/problem_2.cpp
@@ -1,15 +1,13 @@
 float problemSolution2(float i, float j, float k) {
     float result;
     // write your code here
-    double price1, price2, price3;
-    std::cin >> price1;
+    std::cin >> i;
 
-    std::cin >> price2;
+    std::cin >> j;
 
-    std::cin >> price3;
+    std::cin >> k;
 
-    double result = (price1 > price2) ? ((price1 > price3) ? price1 : price3) : ((price2 > price3) ? price2 : price3);
-
+    double result = (i > j) ? ((i > k) ? i : k) : ((j > k) ? j : k);
 
     return result;
 }
