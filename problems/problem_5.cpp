46d2c5423f644f30ade0b180b1cbda7211083a6b
KuroloffS <147370742+KuroloffS@users.noreply.github.com>
2023-11-14 20:40:00 +0500

    Update problem_5.cpp

--- problems/problem_5.cpp
+++ problems/problem_5.cpp
@@ -1,6 +1,17 @@
 float problemSolution5(float x, float y, char operation) {
    float result;
-   // write your code here
+   if (char = '+'){
+      result = x + y;
+   }
+   else if (char = '-'){
+      result = x - y;
+   }
+   else if (char = '*'){
+      result = x * y;
+   }
+   else if (char = '/'){
+      result = x / y;
+   }
 
    return result;
 }
